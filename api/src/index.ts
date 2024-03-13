3d1b498aaa85aae5fa0997849fcdaeca19aab24f
Julia Sanchez <2509juliasanchez@gmail.com>
2024-03-13 20:30:37 -0300

    feat: 🎸 add cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes/routes';
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(express.json());
+app.use(cors())
 app.use('/', routes);
 
 app.listen(port, () => console.log(`App is running on port ${port}`));
